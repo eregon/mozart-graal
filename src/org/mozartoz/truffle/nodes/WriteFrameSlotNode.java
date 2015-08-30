3f9554063fbe3752de48772a64965279c719b0d1
Benoit Daloze <eregontp@gmail.com>
2015-08-30 21:39:30 +0200

    Proper read/write types in the frame

---
+++ src/org/mozartoz/truffle/nodes/WriteFrameSlotNode.java
@@ -0,0 +1,50 @@
+package org.mozartoz.truffle.nodes;
+
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.ImportStatic;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.Frame;
+import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.api.frame.FrameSlotKind;
+import com.oracle.truffle.api.nodes.Node;
+
+@ImportStatic(FrameSlotKind.class)
+public abstract class WriteFrameSlotNode extends Node {
+
+	private final FrameSlot slot;
+
+	public WriteFrameSlotNode(FrameSlot slot) {
+		this.slot = slot;
+	}
+
+	public abstract Object executeWrite(Frame frame, Object value);
+
+	@Specialization(guards = "isKind(frame, Long)")
+	protected long writeLong(Frame frame, long value) {
+		frame.setLong(slot, value);
+		return value;
+	}
+
+	@Specialization(contains = "writeLong")
+	protected Object write(Frame frame, Object value) {
+		if (slot.getKind() != FrameSlotKind.Object) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			slot.setKind(FrameSlotKind.Object);
+		}
+		frame.setObject(slot, value);
+		return value;
+	}
+
+	protected boolean isKind(Frame frame, FrameSlotKind kind) {
+		if (slot.getKind() == kind) {
+			return true;
+		} else if (slot.getKind() == FrameSlotKind.Illegal) {
+			CompilerDirectives.transferToInterpreterAndInvalidate();
+			slot.setKind(kind);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
