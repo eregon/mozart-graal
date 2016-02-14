374b4139be4f6e94f572d5d3b56e48a70d63269d
Benoit Daloze <eregontp@gmail.com>
2016-02-15 01:03:16 +0800

    flatten the SequenceNodes

--- src/org/mozartoz/truffle/nodes/SequenceNode.java
+++ src/org/mozartoz/truffle/nodes/SequenceNode.java
@@ -1,6 +1,10 @@
 package org.mozartoz.truffle.nodes;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
@@ -10,7 +14,7 @@ public class SequenceNode extends OzNode {
 	@Children final OzNode[] statements;
 
 	public static OzNode sequence(OzNode... nodes) {
-		return new SequenceNode(nodes);
+		return singleOrSeq(nodes);
 	}
 
 	public static OzNode sequence(OzNode[] nodes, OzNode last) {
@@ -19,7 +23,33 @@ public class SequenceNode extends OzNode {
 		}
 		OzNode[] all = Arrays.copyOf(nodes, nodes.length + 1);
 		all[all.length - 1] = last;
-		return new SequenceNode(all);
+		return singleOrSeq(all);
+	}
+
+	private static OzNode singleOrSeq(OzNode[] nodes) {
+		OzNode[] flat = flatten(nodes);
+		return flat.length == 1 ? flat[0] : new SequenceNode(flat);
+	}
+
+	private static OzNode[] flatten(OzNode[] nodes) {
+		Deque<OzNode> stack = new ArrayDeque<>(nodes.length);
+		for (OzNode node : nodes) {
+			stack.addFirst(node);
+		}
+		List<OzNode> flat = new ArrayList<OzNode>(nodes.length);
+
+		while (!stack.isEmpty()) {
+			OzNode node = stack.removeLast();
+			if (node instanceof SequenceNode) {
+				OzNode[] subs = ((SequenceNode) node).statements;
+				for (int i = subs.length - 1; i >= 0; i--) {
+					stack.addLast(subs[i]);
+				}
+			} else {
+				flat.add(node);
+			}
+		}
+		return flat.toArray(new OzNode[flat.size()]);
 	}
 
 	private SequenceNode(OzNode... statements) {
