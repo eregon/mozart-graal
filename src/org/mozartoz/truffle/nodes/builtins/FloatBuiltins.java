2cfa923ff04eedcd237a607b28ff08d743c140d5
Benoit Daloze <eregontp@gmail.com>
2016-02-20 13:48:04 +0800

    generate Float builtins

---
+++ src/org/mozartoz/truffle/nodes/builtins/FloatBuiltins.java
@@ -0,0 +1,278 @@
+package org.mozartoz.truffle.nodes.builtins;
+
+import org.mozartoz.truffle.nodes.OzNode;
+
+import com.oracle.truffle.api.dsl.GenerateNodeFactory;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+
+public abstract class FloatBuiltins {
+
+	@Builtin(name = "is")
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class IsFloatNode extends OzNode {
+
+		@Specialization
+		Object isFloat(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@Builtin(name = "/")
+	@GenerateNodeFactory
+	@NodeChildren({ @NodeChild("left"), @NodeChild("right") })
+	public static abstract class FloatDivNode extends OzNode {
+
+		@Specialization
+		Object div(Object left, Object right) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChildren({ @NodeChild("left"), @NodeChild("right") })
+	public static abstract class PowNode extends OzNode {
+
+		@Specialization
+		Object pow(Object left, Object right) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class ToIntNode extends OzNode {
+
+		@Specialization
+		Object toInt(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class AcosNode extends OzNode {
+
+		@Specialization
+		Object acos(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class AcoshNode extends OzNode {
+
+		@Specialization
+		Object acosh(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class AsinNode extends OzNode {
+
+		@Specialization
+		Object asin(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class AsinhNode extends OzNode {
+
+		@Specialization
+		Object asinh(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class AtanNode extends OzNode {
+
+		@Specialization
+		Object atan(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class AtanhNode extends OzNode {
+
+		@Specialization
+		Object atanh(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChildren({ @NodeChild("left"), @NodeChild("right") })
+	public static abstract class Atan2Node extends OzNode {
+
+		@Specialization
+		Object atan2(Object left, Object right) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class CeilNode extends OzNode {
+
+		@Specialization
+		Object ceil(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class CosNode extends OzNode {
+
+		@Specialization
+		Object cos(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class CoshNode extends OzNode {
+
+		@Specialization
+		Object cosh(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class ExpNode extends OzNode {
+
+		@Specialization
+		Object exp(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class FloorNode extends OzNode {
+
+		@Specialization
+		Object floor(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class LogNode extends OzNode {
+
+		@Specialization
+		Object log(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChildren({ @NodeChild("left"), @NodeChild("right") })
+	public static abstract class FModNode extends OzNode {
+
+		@Specialization
+		Object fMod(Object left, Object right) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class RoundNode extends OzNode {
+
+		@Specialization
+		Object round(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class SinNode extends OzNode {
+
+		@Specialization
+		Object sin(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class SinhNode extends OzNode {
+
+		@Specialization
+		Object sinh(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class SqrtNode extends OzNode {
+
+		@Specialization
+		Object sqrt(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class TanNode extends OzNode {
+
+		@Specialization
+		Object tan(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class TanhNode extends OzNode {
+
+		@Specialization
+		Object tanh(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+}
