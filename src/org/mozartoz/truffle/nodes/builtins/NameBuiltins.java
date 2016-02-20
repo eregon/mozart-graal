f417f389d996f8e87f84f4047a5f8afe89c59411
Benoit Daloze <eregontp@gmail.com>
2016-02-20 13:45:20 +0800

    generate Name builtins

---
+++ src/org/mozartoz/truffle/nodes/builtins/NameBuiltins.java
@@ -0,0 +1,79 @@
+package org.mozartoz.truffle.nodes.builtins;
+
+import org.mozartoz.truffle.nodes.OzNode;
+
+import com.oracle.truffle.api.dsl.GenerateNodeFactory;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+
+public abstract class NameBuiltins {
+
+	@Builtin(name = "new")
+	@GenerateNodeFactory
+	public static abstract class NewNameNode extends OzNode {
+
+		@Specialization
+		Object newName() {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("uuid")
+	public static abstract class NewWithUUIDNode extends OzNode {
+
+		@Specialization
+		Object newWithUUID(Object uuid) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("atom")
+	public static abstract class NewUniqueNode extends OzNode {
+
+		@Specialization
+		Object newUnique(Object atom) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChild("printName")
+	public static abstract class NewNamedNode extends OzNode {
+
+		@Specialization
+		Object newNamed(Object printName) {
+			return unimplemented();
+		}
+
+	}
+
+	@GenerateNodeFactory
+	@NodeChildren({ @NodeChild("printName"), @NodeChild("uuid") })
+	public static abstract class NewNamedWithUUIDNode extends OzNode {
+
+		@Specialization
+		Object newNamedWithUUID(Object printName, Object uuid) {
+			return unimplemented();
+		}
+
+	}
+
+	@Builtin(name = "is")
+	@GenerateNodeFactory
+	@NodeChild("value")
+	public static abstract class IsNameNode extends OzNode {
+
+		@Specialization
+		Object isName(Object value) {
+			return unimplemented();
+		}
+
+	}
+
+}
