5647a60875fbbb3f9d34bf9ac4090a8492b45c1f
Benoit Daloze <eregontp@gmail.com>
2016-02-22 16:34:45 +0800

    default pattern

--- vm/src/org/mozartoz/truffle/nodes/builtins/Builtin.java
+++ vm/src/org/mozartoz/truffle/nodes/builtins/Builtin.java
@@ -1,5 +1,6 @@
 package org.mozartoz.truffle.nodes.builtins;
 
+import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,6 +9,22 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Builtin {
+
+	public static final Builtin DEFAULT = new Builtin() {
+		@Override
+		public Class<? extends Annotation> annotationType() {
+			return Builtin.class;
+		}
+
+		@Override
+		public String name() {
+			return "";
+		}
+
+		public boolean proc() {
+			return false;
+		};
+	};
 
 	String name() default "";
 
