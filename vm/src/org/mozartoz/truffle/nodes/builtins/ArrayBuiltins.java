7bcc27464cde4be0dc98a309a7a837b16991f255
Benoit Daloze <eregontp@gmail.com>
2016-03-05 23:39:30 +0100

    pass state test

--- vm/src/org/mozartoz/truffle/nodes/builtins/ArrayBuiltins.java
+++ vm/src/org/mozartoz/truffle/nodes/builtins/ArrayBuiltins.java
@@ -1,6 +1,11 @@
 package org.mozartoz.truffle.nodes.builtins;
 
+import static org.mozartoz.truffle.nodes.builtins.Builtin.ALL;
+
 import org.mozartoz.truffle.nodes.OzNode;
+import org.mozartoz.truffle.nodes.builtins.ArrayBuiltinsFactory.ArrayGetNodeFactory;
+import org.mozartoz.truffle.nodes.builtins.ArrayBuiltinsFactory.ArrayPutNodeFactory;
+import org.mozartoz.truffle.runtime.OzArray;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeChild;
@@ -9,14 +14,24 @@ import com.oracle.truffle.api.dsl.Specialization;
 
 public abstract class ArrayBuiltins {
 
-	@Builtin(name = "new")
+	public static int long2int(long value) {
+		if (Integer.MIN_VALUE <= value && value <= Integer.MAX_VALUE) {
+			return (int) value;
+		} else {
+			throw new Error();
+		}
+	}
+
+	@Builtin(name = "new", deref = { 1, 2 }, tryDeref = 3)
 	@GenerateNodeFactory
 	@NodeChildren({ @NodeChild("low"), @NodeChild("high"), @NodeChild("initValue") })
 	public static abstract class NewArrayNode extends OzNode {
 
 		@Specialization
-		Object newArray(Object low, Object high, Object initValue) {
-			return unimplemented();
+		OzArray newArray(long low, long high, Object initValue) {
+			assert high >= low;
+			int width = long2int(high - low + 1);
+			return new OzArray(long2int(low), width, initValue);
 		}
 
 	}
@@ -55,25 +70,39 @@ public abstract class ArrayBuiltins {
 
 	}
 
+	@Builtin(name = "get", deref = ALL)
 	@GenerateNodeFactory
 	@NodeChildren({ @NodeChild("array"), @NodeChild("index") })
-	public static abstract class GetNode extends OzNode {
+	public static abstract class ArrayGetNode extends OzNode {
+
+		public static ArrayGetNode create() {
+			return ArrayGetNodeFactory.create(null, null);
+		}
+
+		public abstract Object executeGet(OzArray array, long index);
 
 		@Specialization
-		Object get(Object array, Object index) {
-			return unimplemented();
+		Object get(OzArray array, long index) {
+			return array.get(long2int(index));
 		}
 
 	}
 
-	@Builtin(proc = true)
+	@Builtin(name = "put", proc = true, deref = { 1, 2 }, tryDeref = 3)
 	@GenerateNodeFactory
 	@NodeChildren({ @NodeChild("array"), @NodeChild("index"), @NodeChild("newValue") })
-	public static abstract class PutNode extends OzNode {
+	public static abstract class ArrayPutNode extends OzNode {
+
+		public static ArrayPutNode create() {
+			return ArrayPutNodeFactory.create(null, null, null);
+		}
+
+		public abstract Object executePut(OzArray array, long index, Object newValue);
 
 		@Specialization
-		Object put(Object array, Object index, Object newValue) {
-			return unimplemented();
+		Object put(OzArray array, long index, Object newValue) {
+			array.set(long2int(index), newValue);
+			return unit;
 		}
 
 	}
