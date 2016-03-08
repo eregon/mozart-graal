83d3e1656906d77048b127ceb58730c28002e02e
Benoit Daloze <eregontp@gmail.com>
2016-03-08 09:44:41 +0100

    pass conversion test

--- src/org/mozartoz/truffle/nodes/OzNode.java
+++ src/org/mozartoz/truffle/nodes/OzNode.java
@@ -1,5 +1,6 @@
 package org.mozartoz.truffle.nodes;
 
+import org.mozartoz.truffle.runtime.Arity;
 import org.mozartoz.truffle.runtime.OzException;
 import org.mozartoz.truffle.runtime.Unit;
 
@@ -7,6 +8,8 @@ import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.TypeSystemReference;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.object.DynamicObject;
+import com.oracle.truffle.api.object.DynamicObjectFactory;
 
 @TypeSystemReference(OzTypes.class)
 @ImportStatic(OzGuards.class)
@@ -18,6 +21,22 @@ public abstract class OzNode extends Node {
 
 	protected Object unimplemented() {
 		throw new OzException(this, "unimplemented");
+	}
+
+	// Exception helpers
+
+	private static final DynamicObjectFactory KERNEL_ERROR_FACTORY2 = Arity.build("kernel", 1L, 2L).createFactory();
+
+	protected OzException kernelError(String kind, Object arg) {
+		DynamicObject data = OzException.newError(KERNEL_ERROR_FACTORY2.newInstance("kernel", kind, arg));
+		return new OzException(this, data);
+	}
+
+	private static final DynamicObjectFactory KERNEL_ERROR_FACTORY3 = Arity.build("kernel", 1L, 2L, 3L).createFactory();
+
+	protected OzException kernelError(String kind, Object arg1, Object arg2) {
+		DynamicObject data = OzException.newError(KERNEL_ERROR_FACTORY3.newInstance("kernel", kind, arg1, arg2));
+		return new OzException(this, data);
 	}
 
 }
