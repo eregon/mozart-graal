b023568566b31c78193fdb651131f7de9ade82c5
Benoit Daloze <eregontp@gmail.com>
2016-03-24 23:36:20 +0100

    Simple Time.alarm

--- vm/src/org/mozartoz/truffle/nodes/builtins/TimeBuiltins.java
+++ vm/src/org/mozartoz/truffle/nodes/builtins/TimeBuiltins.java
@@ -1,20 +1,41 @@
 package org.mozartoz.truffle.nodes.builtins;
 
+import static org.mozartoz.truffle.nodes.builtins.Builtin.ALL;
+
 import org.mozartoz.truffle.nodes.OzNode;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.coro.Coroutine;
 
 public abstract class TimeBuiltins {
 
+	@Builtin(deref = ALL)
 	@GenerateNodeFactory
 	@NodeChild("delay")
 	public static abstract class AlarmNode extends OzNode {
 
 		@Specialization
-		Object alarm(Object delay) {
-			return unimplemented();
+		Object alarm(long delay) {
+			long end = now() + delay;
+			while (now() < end) {
+				sleep();
+				Coroutine.yield();
+			}
+			return unit;
+		}
+
+		private long now() {
+			return System.currentTimeMillis();
+		}
+
+		private void sleep() {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
