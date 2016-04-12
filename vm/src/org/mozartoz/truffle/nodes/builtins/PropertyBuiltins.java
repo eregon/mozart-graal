e57bd25e780135de94aa1bcf56dd6c9fd82cea7f
Benoit Daloze <eregontp@gmail.com>
2016-04-12 11:02:02 +0200

    Move property management in their own class.

--- vm/src/org/mozartoz/truffle/nodes/builtins/PropertyBuiltins.java
+++ vm/src/org/mozartoz/truffle/nodes/builtins/PropertyBuiltins.java
@@ -2,13 +2,9 @@ package org.mozartoz.truffle.nodes.builtins;
 
 import static org.mozartoz.truffle.nodes.builtins.Builtin.ALL;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.mozartoz.truffle.nodes.OzNode;
-import org.mozartoz.truffle.runtime.OzCons;
 import org.mozartoz.truffle.runtime.OzVar;
-import org.mozartoz.truffle.translator.Loader;
+import org.mozartoz.truffle.runtime.PropertyRegistry;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeChild;
@@ -17,70 +13,7 @@ import com.oracle.truffle.api.dsl.Specialization;
 
 public abstract class PropertyBuiltins {
 
-	private static final Map<String, Object> PROPERTIES = new HashMap<>();
-
-	static {
-		PROPERTIES.put("platform.os", System.getProperty("os.name").intern());
-		PROPERTIES.put("platform.name", System.getProperty("os.name").intern());
-
-		PROPERTIES.put("oz.home", Loader.PROJECT_ROOT.intern());
-		PROPERTIES.put("oz.version", "3.0.0-alpha");
-		PROPERTIES.put("oz.search.path", ".");
-		PROPERTIES.put("oz.search.load", ("cache=/usr/share/mozart/cache").intern());
-
-		PROPERTIES.put("limits.bytecode.xregisters", 65536L);
-		PROPERTIES.put("limits.int.max", Long.MAX_VALUE);
-		PROPERTIES.put("limits.int.min", Long.MIN_VALUE);
-
-		PROPERTIES.put("application.gui", false);
-
-		PROPERTIES.put("errors.debug", true);
-		PROPERTIES.put("errors.depth", 10L);
-		PROPERTIES.put("errors.width", 20L);
-		PROPERTIES.put("errors.thread", 40L);
-
-		PROPERTIES.put("priorities.high", 10L);
-		PROPERTIES.put("priorities.medium", 10L);
-
-		PROPERTIES.put("gc.active", 0L);
-		PROPERTIES.put("gc.codeCycles", 0L);
-		PROPERTIES.put("gc.free", 75L);
-		PROPERTIES.put("gc.on", true);
-		PROPERTIES.put("gc.tolerance", 10L);
-		long MB = 1024L * 1024L;
-		PROPERTIES.put("gc.min", 16L * MB);
-		PROPERTIES.put("gc.size", 32L * MB);
-		PROPERTIES.put("gc.threshold", 48L * MB);
-
-		PROPERTIES.put("messages.gc", false);
-		PROPERTIES.put("messages.idle", false);
-
-		PROPERTIES.put("threads.created", 1L);
-		PROPERTIES.put("threads.runnable", 1L);
-		PROPERTIES.put("threads.min", 1L);
-
-		PROPERTIES.put("time.user", 0L);
-		PROPERTIES.put("time.system", 0L);
-		PROPERTIES.put("time.total", 0L);
-		PROPERTIES.put("time.run", 0L);
-		PROPERTIES.put("time.idle", 0L);
-		PROPERTIES.put("time.copy", 0L);
-		PROPERTIES.put("time.propagate", 0L);
-		PROPERTIES.put("time.gc", 0L);
-		PROPERTIES.put("time.detailed", false);
-	}
-
-	public static void setApplicationURL(String appURL) {
-		PROPERTIES.put("application.url", appURL);
-	}
-
-	public static void setApplicationArgs(String[] args) {
-		Object list = "nil";
-		for (int i = args.length - 1; i >= 0; i--) {
-			list = new OzCons(args[i].intern(), list);
-		}
-		PROPERTIES.put("application.args", list);
-	}
+	private static final PropertyRegistry REGISTRY = PropertyRegistry.INSTANCE;
 
 	@Builtin(proc = true, deref = ALL)
 	@GenerateNodeFactory
@@ -89,8 +22,7 @@ public abstract class PropertyBuiltins {
 
 		@Specialization
 		Object registerValue(String property, Object value) {
-			assert !PROPERTIES.containsKey(property);
-			PROPERTIES.put(property, value);
+			REGISTRY.registerValue(property, value);
 			return unit;
 		}
 
@@ -102,8 +34,9 @@ public abstract class PropertyBuiltins {
 	public static abstract class RegisterConstantNode extends OzNode {
 
 		@Specialization
-		Object registerConstant(Object property, Object value) {
-			return unimplemented();
+		Object registerConstant(String property, Object value) {
+			REGISTRY.registerConstant(property, value);
+			return unit;
 		}
 
 	}
@@ -115,8 +48,9 @@ public abstract class PropertyBuiltins {
 
 		@Specialization
 		boolean get(String property, OzVar result) {
-			if (PROPERTIES.containsKey(property)) {
-				result.bind(PROPERTIES.get(property));
+			Object value = REGISTRY.get(property);
+			if (value != null) {
+				result.bind(value);
 				return true;
 			} else {
 				result.bind(unit);
@@ -133,8 +67,8 @@ public abstract class PropertyBuiltins {
 
 		@Specialization
 		boolean put(String property, Object value) {
-			if (PROPERTIES.containsKey(property)) {
-				PROPERTIES.put(property, value);
+			if (REGISTRY.containsKey(property)) {
+				REGISTRY.put(property, value);
 				return true;
 			} else {
 				return false;
