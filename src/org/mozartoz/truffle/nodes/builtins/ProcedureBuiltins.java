8bcffcf16dd2528c8a4c1e96f9eef469a2128d55
Benoit Daloze <eregontp@gmail.com>
2016-04-12 13:15:16 +0200

    Simple Procedure.apply

--- src/org/mozartoz/truffle/nodes/builtins/ProcedureBuiltins.java
+++ src/org/mozartoz/truffle/nodes/builtins/ProcedureBuiltins.java
@@ -2,13 +2,21 @@ package org.mozartoz.truffle.nodes.builtins;
 
 import static org.mozartoz.truffle.nodes.builtins.Builtin.ALL;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mozartoz.truffle.nodes.OzNode;
+import org.mozartoz.truffle.runtime.OzArguments;
+import org.mozartoz.truffle.runtime.OzCons;
 import org.mozartoz.truffle.runtime.OzProc;
 
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
 
 public abstract class ProcedureBuiltins {
 
@@ -35,20 +43,25 @@ public abstract class ProcedureBuiltins {
 	public static abstract class ArityNode extends OzNode {
 
 		@Specialization
-		long arity(OzProc procedure) {
-			return procedure.arity;
+		long arity(OzProc proc) {
+			return proc.arity;
 		}
 
 	}
 
-	@Builtin(proc = true)
+	@Builtin(proc = true, deref = ALL)
 	@GenerateNodeFactory
 	@NodeChildren({ @NodeChild("procedure"), @NodeChild("args") })
 	public static abstract class ApplyNode extends OzNode {
 
 		@Specialization
-		Object apply(Object procedure, Object args) {
-			return unimplemented();
+		Object apply(VirtualFrame frame, OzProc proc, OzCons args,
+				@Cached("create()") IndirectCallNode callNode) {
+			List<Object> list = new ArrayList<>();
+			args.forEach(e -> list.add(e));
+			Object[] arguments = list.toArray(new Object[list.size()]);
+			callNode.call(frame, proc.callTarget, OzArguments.pack(proc.declarationFrame, arguments));
+			return unit;
 		}
 
 	}
