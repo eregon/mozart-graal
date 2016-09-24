fcf7bc657bbaef44061624de6f3dc2afc16fb955
Benoit Daloze <eregontp@gmail.com>
2016-09-24 20:43:30 +0200

    [Truffle] Split CallProcNode in CallNode, CallProcNode, CallMethodNode and ExecuteValuesNode.

---
+++ src/org/mozartoz/truffle/nodes/call/CallMethodNode.java
@@ -0,0 +1,63 @@
+package org.mozartoz.truffle.nodes.call;
+
+import org.mozartoz.truffle.nodes.DerefNode;
+import org.mozartoz.truffle.nodes.OzGuards;
+import org.mozartoz.truffle.nodes.OzNode;
+import org.mozartoz.truffle.nodes.builtins.RecordBuiltins.LabelNode;
+import org.mozartoz.truffle.runtime.Arity;
+import org.mozartoz.truffle.runtime.OzArguments;
+import org.mozartoz.truffle.runtime.OzObject;
+import org.mozartoz.truffle.runtime.OzProc;
+import org.mozartoz.truffle.runtime.OzUniqueName;
+import org.mozartoz.truffle.runtime.RecordFactory;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
+import com.oracle.truffle.api.object.DynamicObject;
+
+@NodeChildren({ @NodeChild("object"), @NodeChild("arguments") })
+public abstract class CallMethodNode extends OzNode {
+
+	public static CallMethodNode create() {
+		return CallMethodNodeGen.create(null, null);
+	}
+
+	public abstract Object executeCall(VirtualFrame frame, OzObject self, Object[] args);
+
+	static final RecordFactory OTHERWISE_MESSAGE_FACTORY = Arity.build("otherwise", 1L).createFactory();
+
+	@Specialization
+	protected Object callObject(VirtualFrame frame, OzObject self, Object[] args,
+			@Cached("create()") IndirectCallNode callNode,
+			@Cached("create()") DerefNode derefNode,
+			@Cached("create()") LabelNode labelNode) {
+		assert args.length == 1;
+		Object message = derefNode.executeDeref(args[0]);
+
+		final Object name = labelNode.executeLabel(message);
+		assert OzGuards.isLiteral(name);
+
+		OzProc method = getMethod(self, name);
+		if (method == null) { // redirect to otherwise
+			method = getMethod(self, "otherwise");
+			message = OTHERWISE_MESSAGE_FACTORY.newRecord(message);
+		}
+
+		Object[] arguments = new Object[] { self, message };
+		return callNode.call(frame, method.callTarget, OzArguments.pack(method.declarationFrame, arguments));
+	}
+
+	static final OzUniqueName ooMeth = OzUniqueName.get("ooMeth");
+
+	@TruffleBoundary
+	private OzProc getMethod(OzObject self, Object name) {
+		DynamicObject methods = (DynamicObject) self.getClazz().get(ooMeth);
+		return (OzProc) methods.get(name);
+	}
+
+}
--- src/org/mozartoz/truffle/translator/InitFunctor.java
+++ src/org/mozartoz/truffle/translator/InitFunctor.java
@@ -5,7 +5,7 @@ import org.mozartoz.truffle.nodes.OzRootNode;
 import org.mozartoz.truffle.nodes.TopLevelHandlerNode;
 import org.mozartoz.truffle.nodes.builtins.BuiltinsManager;
 import org.mozartoz.truffle.nodes.builtins.ValueBuiltinsFactory.DotNodeFactory;
-import org.mozartoz.truffle.nodes.call.CallProcNodeGen;
+import org.mozartoz.truffle.nodes.call.CallNodeGen;
 import org.mozartoz.truffle.nodes.literal.LiteralNode;
 import org.mozartoz.truffle.runtime.Arity;
 import org.mozartoz.truffle.runtime.OzRecord;
@@ -21,9 +21,9 @@ public abstract class InitFunctor {
 				Arity.build("import", "Boot"),
 				BuiltinsManager.getBootModule("Boot_Boot"));
 
-		OzNode node = CallProcNodeGen.create(
-				new OzNode[] { new LiteralNode(imports), new LiteralNode(new OzVar()) },
-				DotNodeFactory.create(new LiteralNode(initFunctor), new LiteralNode("apply")));
+		OzNode node = CallNodeGen.create(
+				DotNodeFactory.create(new LiteralNode(initFunctor), new LiteralNode("apply")),
+				new LiteralNode(new Object[] { imports, new OzVar() }));
 
 		SourceSection sourceSection = SourceSection.createUnavailable("main", "Init.apply");
 		return new OzRootNode(sourceSection, new FrameDescriptor(), new TopLevelHandlerNode(node), 0);
