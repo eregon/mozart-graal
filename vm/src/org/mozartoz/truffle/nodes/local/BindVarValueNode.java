ba92d5e34ac89c7d9f3ca56c82b5874dcc95d3d1
Benoit Daloze <eregontp@gmail.com>
2016-10-08 21:20:15 +0200

    Optimize binding a variable to a value by speculating on the number of linked variables

--- vm/src/org/mozartoz/truffle/nodes/local/BindVarValueNode.java
+++ vm/src/org/mozartoz/truffle/nodes/local/BindVarValueNode.java
@@ -2,6 +2,7 @@ package org.mozartoz.truffle.nodes.local;
 
 import org.mozartoz.truffle.nodes.OzNode;
 import org.mozartoz.truffle.runtime.OzVar;
+import org.mozartoz.truffle.runtime.Variable;
 
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeChildren;
@@ -10,10 +11,38 @@ import com.oracle.truffle.api.dsl.Specialization;
 @NodeChildren({ @NodeChild("var"), @NodeChild("value") })
 public abstract class BindVarValueNode extends OzNode {
 
-	@Specialization(guards = "!isVariable(value)")
+	public static BindVarValueNode create() {
+		return BindVarValueNodeGen.create(null, null);
+	}
+
+	public abstract Object executeBindVarValue(OzVar var, Object value);
+
+	@Specialization(guards = "var.getNext() == var")
+	Object bind1(Variable var, Object value) {
+		var.setInternalValue(value, var);
+		return value;
+	}
+
+	@Specialization(guards = "var.getNext().getNext() == var")
+	Object bind2(Variable var, Object value) {
+		var.setInternalValue(value, var);
+		var.getNext().setInternalValue(value, var);
+		return value;
+	}
+
+	@Specialization(guards = "var.getNext().getNext().getNext() == var")
+	Object bind3(Variable var, Object value) {
+		var.setInternalValue(value, var);
+		Variable next = var.getNext();
+		next.setInternalValue(value, var);
+		next.getNext().setInternalValue(value, var);
+		return value;
+	}
+
+	@Specialization(contains = { "bind1", "bind2", "bind3" })
 	Object bindLeft(OzVar var, Object value) {
 		var.bind(value);
-		return unit;
+		return value;
 	}
 
 }
