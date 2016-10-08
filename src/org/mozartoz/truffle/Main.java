20d4a80464ece077faeb759e05531bd8dfd05a9e
Benoit Daloze <eregontp@gmail.com>
2016-10-08 13:14:32 +0200

    Add tail recursion tests to the default test suite

--- src/org/mozartoz/truffle/Main.java
+++ src/org/mozartoz/truffle/Main.java
@@ -22,6 +22,7 @@ public class Main {
 			BASE_TESTS + "type.oz",
 			BASE_TESTS + "byneed.oz",
 			BASE_TESTS + "future.oz",
+			BASE_TESTS + "tailrec.oz",
 	};
 
 	public static void main(String[] args) {
