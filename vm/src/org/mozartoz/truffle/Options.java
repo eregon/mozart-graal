a3f4652896f5babf72522069405680fc308849ad
Benoit Daloze <eregontp@gmail.com>
2016-10-08 21:34:26 +0200

    Add an option to disable tail calls

--- vm/src/org/mozartoz/truffle/Options.java
+++ vm/src/org/mozartoz/truffle/Options.java
@@ -6,4 +6,10 @@ public abstract class Options {
 
 	public static final String SHOW_PROC_AST = System.getProperty("oz.show.ast");
 
+	public static final boolean TAIL_CALLS = bool("oz.tail.calls", true);
+
+	private static boolean bool(String property, boolean defaultValue) {
+		return Boolean.valueOf(System.getProperty(property, Boolean.toString(defaultValue)));
+	}
+
 }
