83715fda7e234f88b956c3d4c4e9ec5f3e3cb704
Benoit Daloze <eregontp@gmail.com>
2016-10-21 18:15:38 +0200

    Correctly print #-tuples

--- vm/src/org/mozartoz/truffle/runtime/RecordObjectType.java
+++ vm/src/org/mozartoz/truffle/runtime/RecordObjectType.java
@@ -12,12 +12,25 @@ public class RecordObjectType extends ObjectType {
 	public String toString(DynamicObject record) {
 		Object label = Arity.LABEL_LOCATION.get(record);
 		StringBuilder builder = new StringBuilder();
-		builder.append(label).append('(');
-		for (Property property : record.getShape().getProperties()) {
-			Object value = property.get(record, record.getShape());
-			builder.append(property.getKey()).append(':').append(value).append(' ');
+		Arity arity = OzRecord.getArity(record);
+
+		if (label == "#" && arity.isTupleArity() && arity.getWidth() >= 2) {
+			int width = arity.getWidth();
+			for (long i = 1; i <= width; i++) {
+				builder.append(record.get(i));
+				if (i != width) {
+					builder.append("#");
+				}
+			}
+		} else {
+			builder.append(label).append('(');
+			for (Property property : record.getShape().getProperties()) {
+				Object value = property.get(record, record.getShape());
+				builder.append(property.getKey()).append(':').append(value).append(' ');
+			}
+			builder.setCharAt(builder.length() - 1, ')');
 		}
-		builder.setCharAt(builder.length() - 1, ')');
+
 		return builder.toString();
 	}
 
