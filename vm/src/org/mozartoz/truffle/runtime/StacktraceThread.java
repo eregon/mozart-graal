d9037d871017b31fbd05f34a8b8d7d698424e5d3
Benoit Daloze <eregontp@gmail.com>
2016-10-28 17:19:43 +0200

    Group threads in ByNeedFuture together for stacktrace

--- vm/src/org/mozartoz/truffle/runtime/StacktraceThread.java
+++ vm/src/org/mozartoz/truffle/runtime/StacktraceThread.java
@@ -5,10 +5,21 @@ import java.util.Map.Entry;
 public class StacktraceThread extends Thread {
 	@Override
 	public void run() {
+		int byNeedFutures = 0;
 		for (Entry<OzThread, OzBacktrace> entry : OzThread.BACKTRACES.entrySet()) {
-			System.out.println(entry.getKey().getProc());
-			entry.getValue().showUserBacktrace();
-			System.out.println();
+			OzThread thread = entry.getKey();
+			if (isByNeedFuture(thread)) {
+				byNeedFutures++;
+			} else {
+				System.out.println(thread.getProc());
+				entry.getValue().showUserBacktrace();
+				System.out.println();
+			}
 		}
+		System.out.println(byNeedFutures + " threads in ByNeedFuture");
+	}
+
+	private boolean isByNeedFuture(OzThread thread) {
+		return thread.getProc() != null && thread.getProc().toString().contains("thread in ByNeedFuture in Base.oz");
 	}
 }
