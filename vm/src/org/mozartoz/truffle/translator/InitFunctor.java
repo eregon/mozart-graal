e35a4de10b8026ad15aa8e1f409f2be1a4477125
Benoit Daloze <eregontp@gmail.com>
2016-11-25 16:38:11 +0100

    Use a result variable in InitFunctor#apply()

--- vm/src/org/mozartoz/truffle/translator/InitFunctor.java
+++ vm/src/org/mozartoz/truffle/translator/InitFunctor.java
@@ -1,10 +1,12 @@
 package org.mozartoz.truffle.translator;
 
+import org.mozartoz.truffle.nodes.DerefNode;
 import org.mozartoz.truffle.nodes.OzNode;
 import org.mozartoz.truffle.nodes.OzRootNode;
 import org.mozartoz.truffle.nodes.builtins.BuiltinsManager;
 import org.mozartoz.truffle.nodes.builtins.ValueBuiltinsFactory.DotNodeFactory;
 import org.mozartoz.truffle.nodes.call.CallNode;
+import org.mozartoz.truffle.nodes.control.SequenceNode;
 import org.mozartoz.truffle.nodes.literal.LiteralNode;
 import org.mozartoz.truffle.runtime.Arity;
 import org.mozartoz.truffle.runtime.OzRecord;
@@ -19,10 +21,12 @@ public abstract class InitFunctor {
 				Arity.build("import", "Boot"),
 				BuiltinsManager.getBootModule("Boot_Boot"));
 
-		OzNode node = CallNode.create(
+		OzVar result = new OzVar();
+		OzNode apply = CallNode.create(
 				DotNodeFactory.create(new LiteralNode(initFunctor), new LiteralNode("apply")),
-				new LiteralNode(new Object[] { imports, new OzVar() }));
+				new LiteralNode(new Object[] { imports, result }));
 
+		OzNode node = SequenceNode.sequence(apply, DerefNode.create(new LiteralNode(result)));
 		return OzRootNode.createTopRootNode("Init.apply", node).toCallTarget();
 	}
 
