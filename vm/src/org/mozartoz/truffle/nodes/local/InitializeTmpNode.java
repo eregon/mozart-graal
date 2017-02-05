40e43aa6e69938cdc5a3e22f3cff5798e02334cc
Benoit Daloze <eregontp@gmail.com>
2017-02-05 13:37:55 +0100

    Use a WriteFrameSlotNode in InitializeTmpNode

--- vm/src/org/mozartoz/truffle/nodes/local/InitializeTmpNode.java
+++ vm/src/org/mozartoz/truffle/nodes/local/InitializeTmpNode.java
@@ -5,19 +5,23 @@ import org.mozartoz.truffle.nodes.OzNode;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
-public class InitializeTmpNode extends OzNode {
+public class InitializeTmpNode extends OzNode implements FrameSlotNode {
 
-	final FrameSlot slot;
+	@Child WriteFrameSlotNode writeFrameSlotNode;
 	@Child OzNode value;
 
 	public InitializeTmpNode(FrameSlot slot, OzNode value) {
-		this.slot = slot;
+		this.writeFrameSlotNode = WriteFrameSlotNodeGen.create(slot);
 		this.value = value;
+	}
+
+	public FrameSlot getSlot() {
+		return writeFrameSlotNode.getSlot();
 	}
 
 	@Override
 	public Object execute(VirtualFrame frame) {
-		frame.setObject(slot, value.execute(frame));
+		writeFrameSlotNode.executeWrite(frame, value.execute(frame));
 		return unit;
 	}
 
