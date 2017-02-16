986aaf50d7a75a5daa0b6daeef50e623ce050a3b
Benoit Daloze <eregontp@gmail.com>
2017-02-16 17:46:22 +0100

    Use bool() for most options

--- vm/src/org/mozartoz/truffle/Options.java
+++ vm/src/org/mozartoz/truffle/Options.java
@@ -2,7 +2,7 @@ package org.mozartoz.truffle;
 
 public abstract class Options {
 
-	public static final boolean MEASURE_STARTUP = System.getProperty("oz.measure.startup") != null;
+	public static final boolean MEASURE_STARTUP = bool("oz.measure.startup", false);
 
 	public static final String SHOW_PROC_AST = System.getProperty("oz.show.ast");
 
@@ -15,7 +15,7 @@ public abstract class Options {
 	public static final boolean SELF_TAIL_CALLS = bool("oz.tail.selfcalls", true);
 	public static final boolean SELF_TAIL_CALLS_OSR = bool("oz.tail.selfcalls.osr", true);
 
-	public static final boolean STACKTRACE_ON_INTERRUPT = System.getProperty("oz.stacktrace.on_interrupt") != null;
+	public static final boolean STACKTRACE_ON_INTERRUPT = bool("oz.stacktrace.on_interrupt", false);
 
 	public static final boolean FREE_LINKS = bool("oz.free.links", true);
 	public static final boolean PRINT_NLINKS = bool("oz.print.nlinks", false);
@@ -31,7 +31,7 @@ public abstract class Options {
 		} else if (value.equalsIgnoreCase("false")) {
 			return false;
 		}
-		throw new RuntimeException(property + " was expected to be true or false, got " + value);
+		throw new RuntimeException(property + " was expected to be true or false, got '" + value + "'");
 	}
 
 	private static int integer(String property, int defaultValue) {
