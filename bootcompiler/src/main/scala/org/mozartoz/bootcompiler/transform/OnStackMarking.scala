e26982673951849b214d4a9ec59c54f1b4780d71
mistasse <istassem@gmail.com>
2017-02-27 21:15:50 +0100

    Fix recursion, static analysis for guards and try/catch

--- bootcompiler/src/main/scala/org/mozartoz/bootcompiler/transform/OnStackMarking.scala
+++ bootcompiler/src/main/scala/org/mozartoz/bootcompiler/transform/OnStackMarking.scala
@@ -28,17 +28,17 @@ case class VarAndState(v: Variable, state: VarState)
  * 	This means that any A = {IntToFloat 3} will instantiate because it results in {IntToFloat 3 A}
  *  that could potentially call end up calling A
  *  
- * When having two branches, if a variable does not end in the same state, it it better to instantiate
- * them
+ * When having two branches, if a variable does not end in the same state, it it better to instantiate it
  * 
  * If a variable is encountered, it is always put into the modified set (if any) except if it was already
- * in a BoundFirst or a UsedFirst state.
+ * in a BoundFirst or a ToInstantiate state.
+ * 
+ * In try-catch or andthen/orelse nodes, variables that have not been bound yet end up in ToInstantiate state
  *  
  * The basic idea is that if no usage appears before binding, no need to create a variable.
  */
 class OnStackMarking(val declVars: mutable.HashMap[Symbol, VarAndState],
-    val modified: Option[mutable.Set[Variable]],
-    val onStackBind: ((BindCommon, Variable) => Unit) = {(bind, v) => bind.onStack=true}) extends Transformer with Walker {
+    val modified: Option[mutable.Set[Variable]]) extends Transformer with Walker {
   def this(declVars: mutable.HashMap[Symbol, VarAndState]) = this(declVars, None)
   def this() = this(mutable.HashMap())
   
@@ -47,37 +47,61 @@ class OnStackMarking(val declVars: mutable.HashMap[Symbol, VarAndState],
     modified.foreach { set => set.add(v) }
   }
   
-  def conjugate[A](collector: mutable.HashMap[Symbol, VarAndState], nodes: StatOrExpr*): Unit = {
+  /**
+   * Looks for affected variables using a sandboxed marker
+   */
+  def affectedVariables(affectedSet: mutable.Set[Variable], marking: (OnStackMarking)=>Unit): mutable.Set[Variable] = {
+    val affected = if (affectedSet == null) mutable.Set[Variable]() else affectedSet
+    val marker = new OnStackMarking(declVars.clone(), Some(affected)) {
+      // do nothing when encountering a "bond on stack" node. Bound and Used variables will get the same treatment
+      override def onStackBind(bind: BindCommon, v: Variable) = {}
+    }
+    marking(marker)
+    affected
+  }
+  
+  /**
+   * Runs the algorithm in sandboxed branches and merges the result in current marker
+   */
+  def conjugate[A](collector: mutable.HashMap[Symbol, VarAndState], branches: ((OnStackMarking)=>Unit)*): Unit = {
     val collectorClone = collector.clone()
     val modified = mutable.Set[Variable]()
     val bindNodes = mutable.HashMap[Symbol, Seq[BindCommon]]()
-    for ((node, i) <- nodes.zipWithIndex) {
+    for ((branch, i) <- branches.zipWithIndex) {
       val current = if (i == 0) collector else collectorClone.clone() 
-    	val marker = new OnStackMarking(current, Some(modified), onStackBind = {
-    	  (bind, v) =>
+    	val marker = new OnStackMarking(current, Some(modified)) {
+        // In the sandboxed environment, we collect "bind on stack" nodes to defer decision
+    	  override def onStackBind(bind: BindCommon, v: Variable) =
     	    bindNodes.put(v.symbol, bindNodes.getOrElse(v.symbol, Seq()) :+ bind)
-    	})
-    	node match {
-      	case stat: Statement  => marker.walkStat(stat)
-      	case expr: Expression => marker.walkExpr(expr)
     	}
+      branch(marker)
       if (i != 0) {
         for (v <- modified) {
           val collectorVS = collector.get(v.symbol).get
           val currentVS = current.get(v.symbol).get
-          if (collectorVS.state != currentVS.state) {
+          if (collectorVS.state != currentVS.state && collectorVS.state != ToInstantiate) {
             collector.put(v.symbol, VarAndState(collectorVS.v, ToInstantiate))
           }
         }
       }
     }
+    this.modified.foreach { _ ++= modified }
     for ((sym, bindNodes) <- bindNodes) {
-      if (collector.get(sym).get.state == BoundFirst) {
+      val vas = collector.get(sym).get
+      if (vas.state == BoundFirst) {
         for (bind <- bindNodes) {
-          bind.onStack = true
+          // Now, we confirm the fact those bindNodes can work on the stack
+          onStackBind(bind, vas.v)
         }
       }
     }
+  }
+  
+  /**
+   * The toplevel OnStack markers modify the bind statements and expressions
+   */
+  def onStackBind(bind: BindCommon, v: Variable) = {
+    bind.onStack = true
   }
   
   def bindBindCommon(bind: BindCommon, sym: Symbol, right: Expression) = {
@@ -102,15 +126,22 @@ class OnStackMarking(val declVars: mutable.HashMap[Symbol, VarAndState],
       
     case IfStatement(cond, trueStat, elseStat) =>
       walkExpr(cond)
-      conjugate(declVars, trueStat, elseStat)
+      conjugate(declVars, _.walkStat(trueStat), _.walkStat(elseStat))
       
     case MatchStatement(value, clauses, elseStatement) =>
       walkExpr(value)
-      clauses.map(_.guard).foreach{_ foreach {walkExpr(_)}}
-      conjugate(declVars, ((clauses map (_.body)) :+ elseStatement):_*)
+      conjugate(declVars, (clauses.map {
+        clause =>
+          m: OnStackMarking =>
+           clause.guard foreach {g => m.walkExpr(g)}
+           m.walkStat(clause.body)
+      } :+ {m: OnStackMarking => m.walkStat(elseStatement)}) :_*)
       
     case TryStatement(body, exceptionVar, catchBody) =>
-      conjugate(declVars, body, catchBody)
+      affectedVariables(null, { m =>
+        m.walkStat(body)
+        m.walkStat(catchBody)
+      }) foreach (tagVariable(_, ToInstantiate))
       
     case _ => super.walkStat(statement)
   }
@@ -125,28 +156,33 @@ class OnStackMarking(val declVars: mutable.HashMap[Symbol, VarAndState],
       }
       
     case ProcExpression(name, args, body, flags) =>
-      // The goal is to see what variables it will capture
-      val capturedVariables: mutable.Set[Variable] = mutable.Set()
-      val inProcUsages = new OnStackMarking(declVars.clone(), Some(capturedVariables), onStackBind={(bind, v)=>})
-      inProcUsages.walkStat(body)
-      for (captured <- capturedVariables) {
-        tagVariable(captured, Captured)
-      }
+      affectedVariables(null, { m =>
+        m.walkStat(body)
+      }) foreach (tagVariable(_, Captured))
       
     case IfExpression(cond, trueExpr, elseExpr) =>
       walkExpr(cond)
-      conjugate(declVars, trueExpr, elseExpr)
+      conjugate(declVars, _.walkExpr(trueExpr), _.walkExpr(elseExpr))
       
     case MatchExpression(value, clauses, elseExpression) =>
       walkExpr(value)
-      clauses.map(_.guard).foreach{_ foreach {walkExpr(_)}}
-      conjugate(declVars, ((clauses map (_.body)) :+ elseExpression):_*)
+      conjugate(declVars, (clauses.map { clause =>
+        m: OnStackMarking =>
+         clause.guard foreach {g => m.walkExpr(g)}
+         m.walkExpr(clause.body)
+      } :+ {m: OnStackMarking => m.walkExpr(elseExpression)}):_*)
       
     case TryExpression(body, exceptionVar, catchBody) =>
-      conjugate(declVars, body, catchBody) // TO FIX
+      affectedVariables(null, { m =>
+        m.walkExpr(body)
+        m.walkExpr(catchBody)
+      }) foreach (tagVariable(_, ToInstantiate))
       
     case ShortCircuitBinaryOp(left, op, right) =>
-      conjugate(declVars, left, right) // TO FIX
+      affectedVariables(null, { m =>
+        m.walkExpr(left)
+        m.walkExpr(right)
+      }) foreach (tagVariable(_, ToInstantiate))
       
     case _ => super.walkExpr(expression)
   }
