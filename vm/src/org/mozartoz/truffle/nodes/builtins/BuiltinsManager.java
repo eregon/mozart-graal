90c52d1c1e20bf4bda6e8c45875650e658a57027
Benoit Daloze <eregontp@gmail.com>
2018-10-28 16:17:58 +0100

    Replace reflection in BuiltinsManager with an enum

--- vm/src/org/mozartoz/truffle/nodes/builtins/BuiltinsManager.java
+++ vm/src/org/mozartoz/truffle/nodes/builtins/BuiltinsManager.java
@@ -1,9 +1,11 @@
 package org.mozartoz.truffle.nodes.builtins;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.graalvm.collections.Pair;
 import org.mozartoz.truffle.Options;
 import org.mozartoz.truffle.nodes.DerefIfBoundNode;
 import org.mozartoz.truffle.nodes.DerefNode;
@@ -24,43 +26,49 @@ import com.oracle.truffle.api.source.SourceSection;
 
 public abstract class BuiltinsManager {
 
-	private static final String[] MODULES = {
-			// Base.oz
-			"Value",
-			"Literal",
-			"Cell",
-			"Port",
-			"Atom",
-			"Name",
-			"Int",
-			"Float",
-			"Number",
-			"Tuple",
-			"Procedure",
-			"Dictionary",
-			"Record",
-			"Chunk",
-			"VirtualString",
-			"VirtualByteString",
-			"Coders",
-			"Array",
-			"Object",
-			"Thread",
-			"Exception",
-			"Time",
-			"ForeignPointer",
-			"CompactString",
-			"System",
-			"Property",
-			"WeakReference",
-			// Init.oz
-			"Boot",
-			"OS",
-			"Debug",
-			"Reflection",
-			"Space",
-			"Browser",
-	};
+	enum BuiltinFactories {
+		// Base.oz
+		Value(ValueBuiltinsFactory.getFactories()),
+		Literal(LiteralBuiltinsFactory.getFactories()),
+		Cell(CellBuiltinsFactory.getFactories()),
+		Port(PortBuiltinsFactory.getFactories()),
+		Atom(AtomBuiltinsFactory.getFactories()),
+		Name(NameBuiltinsFactory.getFactories()),
+		Int(IntBuiltinsFactory.getFactories()),
+		Float(FloatBuiltinsFactory.getFactories()),
+		Number(NumberBuiltinsFactory.getFactories()),
+		Tuple(TupleBuiltinsFactory.getFactories()),
+		Procedure(ProcedureBuiltinsFactory.getFactories()),
+		Dictionary(DictionaryBuiltinsFactory.getFactories()),
+		Record(RecordBuiltinsFactory.getFactories()),
+		Chunk(ChunkBuiltinsFactory.getFactories()),
+		VirtualString(VirtualStringBuiltinsFactory.getFactories()),
+		VirtualByteString(VirtualByteStringBuiltinsFactory.getFactories()),
+		Coders(CodersBuiltinsFactory.getFactories()),
+		Array(ArrayBuiltinsFactory.getFactories()),
+		Object(ObjectBuiltinsFactory.getFactories()),
+		Thread(ThreadBuiltinsFactory.getFactories()),
+		Exception(ExceptionBuiltinsFactory.getFactories()),
+		Time(TimeBuiltinsFactory.getFactories()),
+		ForeignPointer(ForeignPointerBuiltinsFactory.getFactories()),
+		CompactString(CompactStringBuiltinsFactory.getFactories()),
+		System(SystemBuiltinsFactory.getFactories()),
+		Property(PropertyBuiltinsFactory.getFactories()),
+		WeakReference(WeakReferenceBuiltinsFactory.getFactories()),
+		// Init.oz
+		Boot(BootBuiltinsFactory.getFactories()),
+		OS(OSBuiltinsFactory.getFactories()),
+		Debug(DebugBuiltinsFactory.getFactories()),
+		Reflection(ReflectionBuiltinsFactory.getFactories()),
+		Space(SpaceBuiltinsFactory.getFactories()),
+		Browser(BrowserBuiltinsFactory.getFactories());
+
+		final List<? extends NodeFactory<? extends OzNode>> factories;
+
+		BuiltinFactories(List<? extends NodeFactory<? extends OzNode>> factories) {
+			this.factories = factories;
+		}
+	}
 
 	private static final Map<String, OzProc> BUILTINS = new HashMap<>();
 	private static final Map<String, DynamicObject> BOOT_MODULES = new HashMap<>();
@@ -92,16 +100,8 @@ public abstract class BuiltinsManager {
 	public static void defineBuiltins(OzLanguage language) {
 		assert BUILTINS.isEmpty();
 		String pkg = BuiltinsManager.class.getPackage().getName();
-		for (String module : MODULES) {
-			try {
-				Class<?> moduleFactory = Class.forName(pkg + "." + module + "BuiltinsFactory");
-				Object untypedFactories = moduleFactory.getMethod("getFactories").invoke(null);
-				@SuppressWarnings("unchecked")
-				List<NodeFactory<? extends OzNode>> factories = (List<NodeFactory<? extends OzNode>>) untypedFactories;
-				installBuiltins(language, module, factories);
-			} catch (ReflectiveOperationException e) {
-				throw new Error(e);
-			}
+		for (BuiltinFactories builtinFactories : BuiltinFactories.values()) {
+			installBuiltins(language, builtinFactories.name(), builtinFactories.factories);
 		}
 		BOOT_MODULES.put("Boot_WeakRef", BOOT_MODULES.get("Boot_WeakReference")); // TODO: hack
 	}
@@ -120,7 +120,7 @@ public abstract class BuiltinsManager {
 		return factory.createNode(arguments);
 	}
 
-	private static void installBuiltins(OzLanguage language, String module, List<NodeFactory<? extends OzNode>> factories) {
+	private static void installBuiltins(OzLanguage language, String module, List<? extends NodeFactory<? extends OzNode>> factories) {
 		// The builtins of this module only, indexed by the builtin name
 		Map<String, OzProc> builtins = new HashMap<>(factories.size());
 
