a7349e703783c91483624f4f061aeba57d3a0830
Benoit Daloze <eregontp@gmail.com>
2019-07-03 00:57:38 +0200

    Use RecordLibrary for Literal.is

--- vm/src/org/mozartoz/truffle/nodes/builtins/LiteralBuiltins.java
+++ vm/src/org/mozartoz/truffle/nodes/builtins/LiteralBuiltins.java
@@ -2,12 +2,13 @@ package org.mozartoz.truffle.nodes.builtins;
 
 import static org.mozartoz.truffle.nodes.builtins.Builtin.ALL;
 
+import com.oracle.truffle.api.library.CachedLibrary;
 import org.mozartoz.truffle.nodes.OzNode;
-import org.mozartoz.truffle.runtime.OzName;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
+import org.mozartoz.truffle.runtime.RecordLibrary;
 
 public abstract class LiteralBuiltins {
 
@@ -16,19 +17,10 @@ public abstract class LiteralBuiltins {
 	@NodeChild("value")
 	public static abstract class IsLiteralNode extends OzNode {
 
-		@Specialization
-		boolean isLiteral(String atom) {
-			return true;
-		}
-
-		@Specialization
-		boolean isLiteral(OzName name) {
-			return true;
-		}
-
-		@Specialization(guards = "!isLiteral(value)")
-		boolean isLiteral(Object value) {
-			return false;
+		@Specialization(limit = "RECORDS_LIMIT")
+		boolean isLiteral(Object value,
+				@CachedLibrary("value") RecordLibrary records) {
+			return records.isLiteral(value);
 		}
 
 	}
